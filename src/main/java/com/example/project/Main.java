db910ca7ccbccdf199228122d07f88968d4c0b71
Kadek Dhiva Tiradika <dhivatiradika@gmail.com>
2024-04-30 22:31:53 +0700

    fix: cannot use scanner when run with gradle command

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -1,8 +1,12 @@
 package com.example.project;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Add number: ");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("Ini adalah" + a);
     }
 
     public static int add(int a, int b) {
